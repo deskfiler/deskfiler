6e0ada74dc69e76efe346a631748b6db528b4ff9
Kotyk Roman Viktorovich <sleeporsmile@gmail.com>
2020-02-27 20:36:11 +0200

    fix windows.js

--- src/main-renderer/containers/Modals/index.js
+++ src/main-renderer/containers/Modals/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Flex } from 'styled';
 
-import InstallPluginModal from './InstallPlugin';
+import InstallPluginModal from './InstallModule';
 import ConfirmationModal from './Confirmation';
 import AlertModal from './Alert';
 import AuthModal from './Auth';
